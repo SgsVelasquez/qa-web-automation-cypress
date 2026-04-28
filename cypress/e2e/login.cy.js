1bc1d7168d34a15b3362b320536b76fed3c75f10
Sérgio Velasquez <47956760+SgsVelasquez@users.noreply.github.com>
2026-04-28 18:13:01 -0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,12 +1,24 @@
-describe('SauceDemo - Authentication and Cart Tests', () => {
+describe('SauceDemo - Authentication', () => {
+
   it('Deve realizar login com sucesso', () => {
     cy.visit('https://www.saucedemo.com/')
 
     cy.get('[data-test="username"]').type('standard_user')
     cy.get('[data-test="password"]').type('secret_sauce')
-
     cy.get('[data-test="login-button"]').click()
 
     cy.url().should('include', '/inventory')
+
+    cy.get('.title').should('contain', 'Products')
+  })
+
+  it('Deve exibir erro ao logar com senha inválida', () => {
+    cy.visit('https://www.saucedemo.com/')
+
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('senha_errada')
+    cy.get('[data-test="login-button"]').click()
+
+    cy.get('[data-test="error"]').should('be.visible')
   })
 })
